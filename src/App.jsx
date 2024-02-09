146b34f77679300c781d44dd9fba50a563878dc7
swapnil0x7 <swapnil0x7@gmai.com>
2024-02-09 20:03:43 +0530

    created development branch

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
 	return (
 		<>
-			<div className='p-2 m-2 text-center border border-gray-300'>
+			<div className='p-2 m-2 text-center border border-gray-400'>
 				Welcome to StockInvento
 			</div>
 		</>
