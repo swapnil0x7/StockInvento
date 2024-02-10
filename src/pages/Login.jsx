44ad4d20c0092b8ad03a2caca5a1c18fd8ecfcc3
swapnil0x7 <swapnil0x7@gmai.com>
2024-02-11 00:10:35 +0530

    Added animation component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,31 @@
 import { TypeAnimation } from 'react-type-animation';
-import logo from '../assets/Logo.svg';
+import logoAnimation from '../assets/animations/Logo.json';
 import Form from '../components/Form';
+import Lottie from 'react-lottie';
 
 const Login = () => {
 	return (
 		<main className='grid lg:grid-cols-2'>
-			<div className='h-[100vh] w-full hidden lg:flex flex-col justify-center items-center'>
-				<img className='w-48 h-48' src={logo} alt='logo' />
-				<p className='w-full text-center font-bold text-xl mt-2 text-green-500 whitespace-no-wrap overflow-hidden'>
+			<div className='relative h-[100vh] w-full hidden lg:flex flex-col justify-center items-center'>
+				<Lottie
+					options={{
+						animationData: logoAnimation,
+						loop: true,
+						autoplay: true,
+					}}
+					height={500}
+					width={500}
+				/>
+				<p className='absolute w-full top-[30rem] text-center font-bold text-xs mt-2 text-blue-300 whitespace-no-wrap overflow-hidden'>
 					<TypeAnimation
 						sequence={[
-							'STOCK INVENTO',
+							'INVENTORY MANAGEMENT APPLICATION',
 							1000,
-							'INVENTORY MANAGEMENT',
-							1000,
-							'MODERN INTERFACE',
+							'MODERN USER INTERFACE',
 							1000,
 							'SEAMLESS STOCK MANAGEMENT',
+							1000,
+							'INSTANT ALERTS AND CRITICAL NOTIFICATIONS',
 							1000,
 						]}
 						wrapper='span'
@@ -34,3 +43,25 @@ const Login = () => {
 };
 
 export default Login;
+
+{
+	/* <img className='w-48 h-48' src={logo} alt='logo' />
+				<p className='w-full text-center font-bold text-xl mt-2 text-green-500 whitespace-no-wrap overflow-hidden'>
+					<TypeAnimation
+						sequence={[
+							'STOCK INVENTO',
+							1000,
+							'INVENTORY MANAGEMENT',
+							1000,
+							'MODERN INTERFACE',
+							1000,
+							'SEAMLESS STOCK MANAGEMENT',
+							1000,
+						]}
+						wrapper='span'
+						speed={30}
+						style={{ display: 'inline-block' }}
+						repeat={Infinity}
+					/>
+				</p> */
+}
